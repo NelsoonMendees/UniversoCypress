3a4d60e50fa15c38740ebddbb181ae44e8717834
Nelson Neto <nelson.neto@aliare.co>
2022-05-10 17:31:23 -0300

    Topicos Avançados e Configurações - Aula 3

--- testes/cypress/integration/dash.spec.js
+++ testes/cypress/integration/dash.spec.js
@@ -1,107 +1,34 @@
 ///<reference types="Cypress" />
 import loginPage from '../support/pages/login';
+import dashPage from '../support/pages/dash';
+
+import {custumer, provider, appointment} from '../support/factories/dash'
 
 describe("Dashboard", function () {
 
     context("Quando o cliente faz um agendamento no app mobile", function () {
 
-        const data = {
-            custumer: {
-                name: 'Nikki Sixx',
-                email: 'sixx@cliente.com',
-                password: 'pwd123',
-                is_provider: false
-            },
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'madruga@samuraibs.com',
-                password: 'pwd123',
-                is_provider: true
-            }
-        }
-
         before(function () {
-            cy.postUser(data.provider) //prestador de serviço
-            cy.postUser(data.custumer) //cliente
+            cy.postUser(provider) //reconstrução prestador de serviço
+            cy.postUser(custumer) //reconstrução cliente
 
-            cy.apiLogin(data.custumer) //simula o cliente realizando login no app
-            cy.log('Token capturado ' + Cypress.env('apiToken'))
-
-            cy.setProviderId(data.provider.email)
-            cy.createAppointment()
+            cy.apiLogin(custumer) //simula o cliente realizando login no app
+            cy.setProviderId(provider.email) //pega o id do prestador de serviço
+            cy.createAppointment(appointment.hour) //realiza o agendamento do serviço
         })
 
         it('O mesmo deve ser exibido no dashboard', function () {
 
             loginPage.go()
-            loginPage.form(data.provider)
+            loginPage.form(provider)
             loginPage.submit()
 
-            cy.wait(3000)
+            dashPage.calendarShoudBeVisible()
+
+            const day = Cypress.env('appointmentDay')
+            dashPage.selectDay(day)
+
+            dashPage.appointmentShoudBe(custumer, appointment.hour)
         })
-    })
-})
-
-import moment from 'moment';
-
-Cypress.Commands.add('createAppointment', function () {
-    let now = new Date()
-
-    now.setDate(now.getDate() + 1)
-
-    const date = moment(now).format('YYYY-MM-DD 14:00:00')
-
-    const payload = {
-        provider_id: Cypress.env('providerId'),
-        date: date
-    }
-
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3333/appointments',
-        body: payload,
-        headers: {
-            authorization: 'Bearer ' + Cypress.env('apiToken')
-        }
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-    })
-})
-
-Cypress.Commands.add('setProviderId', function (providerEmail) {
-
-    cy.request({
-        method: 'GET',
-        url: 'http://localhost:3333/providers',
-        headers: {
-            authorization: 'Bearer ' + Cypress.env('apiToken')
-        }
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-        console.log(response.body)
-
-        const providerList = response.body
-
-        providerList.forEach(function (provider) {
-            if (provider.email === providerEmail) {
-                Cypress.env('providerId', provider.id)
-            }
-        })
-    })
-})
-
-Cypress.Commands.add('apiLogin', function (user) {
-    const payload = {
-        email: user.email,
-        password: user.password
-    }
-
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3333/sessions',
-        body: payload
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-        Cypress.env('apiToken', response.body.token) //captura o token gerado
     })
 })
--- testes/cypress/support/pages/dash/index.js
+++ testes/cypress/support/pages/dash/index.js
@@ -1,10 +1,31 @@
-import {el} from './elements'
+import { el } from './elements'
 import header from '../../components/header'
 
 class DashPage {
-constructor(){
-    this.header = header
-}
+    constructor() {
+        this.header = header
+    }
+
+    calendarShoudBeVisible() {
+        cy.get(el.calendar, {
+                timeout: 7000
+            })
+            .should('be.visible')
+    }
+
+    selectDay(day) {
+        const target = new RegExp('^' + day + '$', 'g')
+        cy.contains(el.boxDay, target)
+            .click({force: true})
+    }
+
+    appointmentShoudBe(custumer, hour){
+        cy.contains('div', custumer.name)
+            .should('be.visible')
+            .parent()
+            .contains(el.boxHour, hour)
+            .should('be.visible')
+    }
 }
 
 export default new DashPage()
